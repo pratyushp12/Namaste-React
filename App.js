e347546d1a0c8cf0324c9121e337b45bee2d2a5b
Praveen <pratyush.praveen@accenture.com>
2024-12-20 12:19:50 +0530

    updated nested child elements

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const heading = React.createElement('h1',{},'Hello World from React');
+const heading = React.createElement('div',{id : 'parent'},[
+    React.createElement('h1',{},'I am h1 tag'),
+    React.createElement('h2',{},'I am h2 tag')
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
