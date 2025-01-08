30e73428718a93845c750207807a2e246f90489a
Praveen <pratyush.praveen@accenture.com>
2025-01-09 00:08:51 +0530

    Add script in package.json and add initial JSX

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
+/* React.createElement */
+// const heading = React.createElement('div',{id : 'parent'},[
+//     React.createElement('h1',{},'I am h1 tag'),
+//     React.createElement('h2',{},'I am h2 tag')
+// ]);
 
-const heading = React.createElement('div',{id : 'parent'},[
-    React.createElement('h1',{},'I am h1 tag'),
-    React.createElement('h2',{},'I am h2 tag')
-]);
+
+// JSX
+const Title= ()=> <h1>I am title</h1>
+const JsxHeading = ()=> (
+    <div>
+        <h1>I am heading</h1>
+<Title />
+</div>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<JsxHeading />);
